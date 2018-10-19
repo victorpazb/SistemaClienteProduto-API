8045d43acaea4b69f30acd44ccfb0683a062b111
victorpazb <victor.braga@ccc.ufcg.edu.br>
2018-10-19 19:39:00 -0300

    modificando sexta noite

--- src/Aposta.java
+++ src/Aposta.java
@@ -1,9 +1,38 @@
 
 public class Aposta {
-	
+
 	private String nomeApostador;
 	private int valorAposta;
 	private String previsao;
+
+	
+	public Aposta(String nome, int valor, String previsao) {
+		
+		this.nomeApostador = nome;
+		this.valorAposta = valor;
+		this.previsao = previsao;
+	}
+
+
+	public String getNomeApostador() {
+		return this.nomeApostador;
+	}
+
+
+	public int getValorAposta() {
+		return this.valorAposta;
+	}
+
+
+	public String getPrevisao() {
+		return this.previsao;
+	}
+
+
+	@Override
+	public String toString() {
+		return this.nomeApostador + " - " + this.valorAposta + " - " + this.previsao;
+	}
 	
 	
 
